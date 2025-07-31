8c8034ba021115d28b8de90f052832ef651f0d5f
rolenthedeep <brant@brantmartin.com>
2025-07-31 14:59:03 +0000

    Revert "Merge branch 'experiment/optkg' into develop"
    This reverts commit 272e36be3cfdc28e3f53ca261ac1d594d110677b

--- src/rempack.cpp
+++ src/rempack.cpp
@@ -50,9 +50,6 @@ void setupStyle(){
 
 [[noreturn]]
 void Rempack::startApp() {
-#ifndef NDEBUG
-    raise(SIGINT);
-#endif
     setupStyle();
     fb = framebuffer::get();
     auto scene = buildHomeScene(fb->width, fb->height);
@@ -63,6 +60,7 @@ void Rempack::startApp() {
     //ui::MainLoop::redraw();
 
     setupDebug();
+    filterMgr->updateLists(filterOpts, "");
     while(true){
         auto mstart = chrono::steady_clock::now();
         ui::MainLoop::main();
@@ -151,23 +149,20 @@ void onPreviewClick(void*){
 }
 
 void initScreen(){
-    fb->waveform_mode = WAVEFORM_MODE_INIT;
-    fb->clear_screen();
-    fb->redraw_screen(true);
     fb->draw_rect(0,0,fb->width, fb->height, BLACK);
-    fb->update_mode = UPDATE_MODE_FULL;
-    fb->waveform_mode = WAVEFORM_MODE_GC16;
+    //fb->update_mode = UPDATE_MODE_FULL;
+    //fb->waveform_mode = WAVEFORM_MODE_A2;
     fb->dirty = true;
-    fb->redraw_screen(true);
+    fb->redraw_screen();
     fb->clear_screen();
     //fb->redraw_screen();
-    fb->update_mode = UPDATE_MODE_PARTIAL;
+    //fb->update_mode = UPDATE_MODE_PARTIAL;
     //fb->waveform_mode = WAVEFORM_MODE_GC16;
 }
 
 void setupDebug(){
 #ifndef NDEBUG
-//    std::raise(SIGINT);   //firing a sigint here helps synchronize remote gdbserver
+    std::raise(SIGINT);   //firing a sigint here helps synchronize remote gdbserver
     //sleep(10);
 
     //packagePanel->select("splashscreen-poweroff-sacks_spiral");
@@ -203,6 +198,15 @@ ui::Scene buildHomeScene(int width, int height) {
 
     initScreen();
 
+    //vertical stack that takes up the whole screen
+    auto layout = new ui::VerticalReflow(padding, padding, width - padding*2, height - padding*2, scene);
+
+    opkg::Instance = &pkg;
+    pkg.InitializeRepositories();
+    /* Search + menus */
+    //short full-width pane containing search and menus
+    auto searchPane = new ui::HorizontalReflow(0, 0, layout->w, 80, scene);
+
     filterOpts = make_shared<widgets::FilterOptions>(widgets::FilterOptions{
             .Installed = true,
             .Upgradable = false,
@@ -211,16 +215,9 @@ ui::Scene buildHomeScene(int width, int height) {
             .SearchHidden = true,
             .groupSplash = false,
     });
-
-    //vertical stack that takes up the whole screen
-    auto layout = new ui::VerticalReflow(padding, padding, width - padding*2, height - padding*2, scene);
-
-    opkg::Instance = &pkg;
-    /* Search + menus */
-    //short full-width pane containing search and menus
-    auto searchPane = new ui::HorizontalReflow(0, 0, layout->w, 80, scene);
-
-
+    for(auto &r : pkg.repositories){
+        filterOpts->Repos.emplace(r, r != "entware");   //hide entware by default, there's so many openwrt packages it drowns out toltec
+    }
     auto filterButton = new widgets::FilterButton(0,0,60,60, filterOpts);
     filterButton->events.updated += onFiltersChanged;
     menuData = new widgets::MenuData;
@@ -237,6 +234,10 @@ ui::Scene buildHomeScene(int width, int height) {
     //full-width horizontal stack underneath the search pane. give it half the remaining height
     auto applicationPane = new ui::HorizontalReflow(0, 0, layout->w, (layout->h - searchPane->h - padding)/2, scene);
     filterPanel = new widgets::ListBox(0, 0, 300, applicationPane->h, 45, scene, widgets::LightButtonStyle());
+    std::vector<std::string> sections;
+    pkg.LoadSections(&sections);
+    for (const auto &s: sections)
+        filterPanel->add(s);
 
     filterPanel->events.selected += PLS_DELEGATE(onFilterAdded);
     filterPanel->events.deselected += PLS_DELEGATE(onFilterRemoved);
@@ -247,16 +248,8 @@ ui::Scene buildHomeScene(int width, int height) {
     packagePanel->events.deselected += PLS_DELEGATE(onPackageDeselect);
 
     filterMgr = new ListFilter(filterPanel, packagePanel);
-    pkg.InitializeRepositoriesAsync([=](){
-        ui::IdleQueue::add_task([=]() {
-            for(auto &r : pkg.repositories){
-                filterOpts->Repos.emplace(r, r != "entware");   //hide entware by default, there's so many openwrt packages it drowns out toltec
-            }
-            filterMgr->loadLists(pkg);
-            filterMgr->updateLists(filterOpts, "");
-        });
-        ui::TaskQueue::wakeup(); //wake main thread
-    });
+    filterMgr->updateLists(filterOpts, "");
+
     displayBox = new widgets::PackageInfoPanel(0,0,applicationPane->w,applicationPane->h, widgets::RoundCornerStyle(), scene);
 
     displayBox->events.install += PLS_DELEGATE(onInstallClick);
