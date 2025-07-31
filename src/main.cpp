118e9dc5300c5b2e5b29f0634d89e81496638d26
rempack-b <rm@brantmartin.com>
2025-07-31 10:32:12 -0400

    add system to capture every frame to disk

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <unistd.h>
 
 #include "rempack.h"
+#include "ScreenCatcher.h"
 
-Rempack r;
-int main()
-{
-r.startApp();
-    return 0;
+
+int main() {
+#ifndef CAPTURE_SCREEN
+    Rempack::.startApp();
+#else
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("couldn't open pipe!");
+        return 1;
+    }
+
+    auto pid = fork();
+    if (pid == -1) {
+        perror("error forking!");
+        return 1;
+    }
+
+    if (pid == 0) {
+        close(pipefd[1]);
+        auto sc = ScreenCatcher();
+        auto ret = sc.Listen(pipefd[0]);
+        std::cerr << "ScreenCatcher returned with code " << ret << std::endl;
+        return ret;
+    } else {
+        close(pipefd[0]);
+        Rempack::startApp(pipefd[1]);
+    }
+
+#endif
 }
