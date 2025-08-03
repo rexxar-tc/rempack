93a7284419e15512adee66975e305259c8206e3d
rempack-b <rm@brantmartin.com>
2025-08-03 14:10:24 -0400

    Fix event button crash

--- ui/buttons/event_button.cpp
+++ ui/buttons/event_button.cpp
@@ -5,25 +5,28 @@
 #include "../debug/debug_widgets.h"
 #include <utility>
 namespace widgets {
-    EventButton::EventButton(int x, int y, int w, int h, string text, RoundCornerStyle style): ui::Button(x,y,w,h,text), border_style(style) {
+    EventButton::EventButton(int x, int y, int w, int h, string text, RoundCornerStyle style) : ui::Button(x, y, w, h, text), border_style(style) {
         x_padding = 0;
         y_padding = 0;
         border_style = style;
-        border = make_shared<RoundCornerWidget>(x,y,w,h,style);
+        border = make_shared<RoundCornerWidget>(x, y, w, h, style);
         children.push_back(border);
 
     }
 
-    EventButton::EventButton(int x, int y, int w, int h, RoundCornerStyle style): EventButton(x,y,w,h,"",style) {
+    EventButton::EventButton(int x, int y, int w, int h, RoundCornerStyle style) : EventButton(x, y, w, h, "", style) {
     }
 
     EventButton::EventButton(int x, int y, int w, int h, string text) : Button(x, y, w, h, text) {
         x_padding = 0;
         y_padding = 0;
+        //  border = make_shared<RoundCornerWidget>(x,y,w,h,widgets::LightButtonStyle());
+        //  border->visible = false;
     }
 
     void EventButton::mark_redraw() {
-        border->mark_redraw();
+        if (border)
+            border->mark_redraw();
         Widget::mark_redraw();
     }
 
@@ -37,18 +40,22 @@ namespace widgets {
 
     void EventButton::disable() {
         enabled = false;
-        border->style.startColor = 3.0/31.0f;
+        if (border && borderEnabled)
+            border->style.startColor = 3.0 / 31.0f;
         mark_redraw();
     }
 
     void EventButton::enable() {
         enabled = true;
-        border->style.startColor = border_style.startColor;
+
+        if (border && borderEnabled)
+            border->style.startColor = border_style.startColor;
         mark_redraw();
     }
+
 //e-40c2r4
     void EventButton::render_border() {
-        if(borderEnabled)
+        if (border && borderEnabled)
             border->render_border();
         else
             ui::Widget::render_border();
@@ -62,7 +69,7 @@ namespace widgets {
         if (!enabled) {
             fb->draw_rect(x, y, w, h, color::GRAY_12, true);
         }
-        if(!text.empty())
+        if (!text.empty())
             textWidget->render();
         //fb->draw_rect(x,y,w,h,BLACK,false);
 #ifdef WIDGET_DEBUG
@@ -71,37 +78,45 @@ namespace widgets {
     }
 
     void EventButton::undraw() {
-        border->render_inside_fill();
+        if (border && borderEnabled)
+            border->render_inside_fill();
     }
 
     void EventButton::show() {
-        if(borderEnabled)
+        if (border && borderEnabled)
             border->show();
         Widget::show();
     }
 
     void EventButton::hide() {
-        border->hide();
+        if (border && borderEnabled)
+            border->hide();
         Widget::hide();
     }
 
     void EventButton::enableBorder(bool enable) {
+        if (!border) {
+            borderEnabled = false;
+            return;
+        }
         borderEnabled = enable;
-        if(enable)
+        if (enable)
             border->show();
         else
             border->hide();
     }
 
     void EventButton::debugRender() {
-        fb->draw_rect(x,y,w,h, toRColor(128,0,128),false);
+        fb->draw_rect(x, y, w, h, toRColor(128, 0, 128), false);
         auto t = textWidget;
-        fb->draw_rect(t->x+2, t->y+2, t->w-4,t->h-4, toRColor(0,255,128),false);
+        fb->draw_rect(t->x + 2, t->y + 2, t->w - 4, t->h - 4, toRColor(0, 255, 128), false);
     }
 
     void EventButton::on_reflow() {
-        border->set_coords(x,y,w,h);
-        border->on_reflow();
+        if (border && borderEnabled) {
+            border->set_coords(x, y, w, h);
+            border->on_reflow();
+        }
         Rect::on_reflow();
     }
 
